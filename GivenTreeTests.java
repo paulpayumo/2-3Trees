9b53f35f7a6a90f89b2f1b6a656290341fa8be0e
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-09-27 04:45:06 -0700

    Create GivenTreeTests.java

---
+++ GivenTreeTests.java
@@ -0,0 +1,138 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GivenTreeTests
+{
+
+   @Test
+   public void singleNodeTree()
+   {
+      Tree t = new Tree();
+      t.insert(9);
+      
+      assertEquals(1, t.size(9));
+      assertEquals(0, t.size(8));
+      assertEquals(0, t.size(10));
+      
+      t.insert(15);
+      assertEquals(2, t.size(9));
+      assertEquals(0, t.size(8));
+      assertEquals(0, t.size(10));
+      assertEquals(2, t.size(15));
+      assertEquals(0, t.size(18));
+
+      t = new Tree();
+      t.insert(15);
+      t.insert(9);
+      assertEquals(2, t.size(9));
+      assertEquals(0, t.size(8));
+      assertEquals(0, t.size(10));
+      assertEquals(2, t.size(15));
+      assertEquals(0, t.size(18));
+
+   }
+   
+   @Test
+   public void oneSplitLeft()
+   {
+      Tree t = new Tree();
+      t.insert(9);
+      t.insert(15);
+      t.insert(1);
+      
+      assertEquals(3, t.size(9));
+      assertEquals(1, t.size(15));
+      assertEquals(0, t.size(17));
+      assertEquals(0, t.size(11));
+
+      assertEquals(1, t.size(1));
+      assertEquals(0, t.size(0));
+      assertEquals(0, t.size(3));
+   }
+   
+   @Test
+   public void oneSplitRight()
+   {
+      Tree t = new Tree();
+      t.insert(1);
+      t.insert(9);
+      t.insert(15);
+      
+      assertEquals(3, t.size(9));
+      assertEquals(1, t.size(15));
+      assertEquals(0, t.size(17));
+      assertEquals(0, t.size(11));
+
+      assertEquals(1, t.size(1));
+      assertEquals(0, t.size(0));
+      assertEquals(0, t.size(3));
+   }
+
+   @Test
+   public void oneSplitMiddle()
+   {
+      Tree t = new Tree();
+      t.insert(1);
+      t.insert(15);
+      t.insert(9);
+      
+      assertEquals(3, t.size(9));
+      assertEquals(1, t.size(15));
+      assertEquals(0, t.size(17));
+      assertEquals(0, t.size(11));
+
+      assertEquals(1, t.size(1));
+      assertEquals(0, t.size(0));
+      assertEquals(0, t.size(3));
+   }
+
+   
+   @Test
+   public void testDuplicates()
+   {
+      Tree t = new Tree();
+      t.insert(1);
+      t.insert(9);
+      t.insert(15);
+      t.insert(13);
+      t.insert(20);
+      t.insert(7);
+      t.insert(4);
+      t.insert(1);
+      t.insert(9);
+      t.insert(15);
+      t.insert(1);
+      t.insert(9);
+      t.insert(15);
+      t.insert(13);
+      t.insert(20);
+      t.insert(7);
+      t.insert(4);
+      t.insert(13);
+      t.insert(20);
+      t.insert(7);
+      t.insert(4);
+
+      assertEquals(7, t.size(9));
+      assertEquals(3, t.size(4));
+      assertEquals(3, t.size(15));
+
+      assertEquals(0, t.size(12));
+      assertEquals(1, t.size(13));
+      assertEquals(0, t.size(14));
+      assertEquals(0, t.size(19));
+      assertEquals(1, t.size(20));
+      assertEquals(0, t.size(21));
+
+      assertEquals(1, t.size(1));
+      assertEquals(0, t.size(0));
+      assertEquals(0, t.size(3));
+
+      assertEquals(0, t.size(6));
+      assertEquals(1, t.size(7));
+      assertEquals(0, t.size(8));
+
+   }
+
+
+}
